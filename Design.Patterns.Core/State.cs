2bd9ee135951674a0e22cd6b0f8a79906b349b53
rsibanez89 <rsibanez89@gmail.com>
2019-09-10 23:14:21 +1000

    Added database setup using liquibase

--- Design.Patterns.Core/State.cs
+++ Design.Patterns.Core/State.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Design.Patterns.Core
 {
@@ -11,12 +9,12 @@ namespace Design.Patterns.Core
 		public int Version { get; set; }
 
 		public long LastModifiedBy { get; set; }
-		public DateTime LastModifiedOnUtc { get; set; }
+		public DateTime LastModifiedOn { get; set; }
 
 		public long CreatedBy { get; set; }
-		public DateTime CreatedOnUtc { get; set; }
+		public DateTime CreatedOn { get; set; }
 
 		public long? DeletedBy { get; set; }
-		public DateTime? DeletedOnUTC { get; set; }
+		public DateTime? DeletedOn { get; set; }
 	}
 }
--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Design Patterns in C# code
 It will contain common designs Patterns that I've implemented or used.
 
+# Setup
+## Database
+This project is using SQLite and liquibase.
+Install liquibase-3.5.5 in `C:\opt\liquibase` and add it to the Path environment variable. [LINK](https://download.liquibase.org/download-community/)
+Then Run:
+```
+> cd Design.Patterns.WebApi/sqlite-database/
+> liquibase update
+```
+
 # Useful commands
 `dotnet new webapi -n Design.Patterns.WebApi`
+
